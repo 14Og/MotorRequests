907bc67001b1658a12621d9eac2c7f8d37c19166
14Og <kolya.kiz@yandex.ru>
2023-09-01 17:37:05 +0300

    modified requests structure

--- Stepper-requests.py
+++ Stepper-requests.py
@@ -5,63 +5,90 @@ import json
 import sys
 
 
-class CommandError(SyntaxError):
+class CommandError(Exception):
     def __init__(self, message):
+        message = f"*CommandError*:{message}"
         super().__init__(message)
+
+class ServerError(Exception):
+    def __init__(self, message):
+        message = f"*ServerError*:{message}"
+        super().__init__(message)
+
+
         
         
 class MotorRequest:
-    def __init__(self):
+    def __init__(self) -> None:
+        self.args = sys.argv
         self.hostname = socket.gethostname()
         try:
             self.ip_addr = json.loads(os.popen("ip -j -4 route").read())[0]["prefsrc"]
         except KeyError:
             self.ip_addr = socket.gethostbyname(self.hostname)
+            print("NO WLAN0 CONNECTION!")
         
-    def process_command(self):
+    def load_json_response(self, response):
+        response = json.loads(response.text)
+        return response
+
+    def process_command(self) -> str:
         """
-        motor name - X or Y
-        coordinates - from -90 to 90  
-        command - move or park (G1 or G28)
+        4 types of commands:
+         -starting command
+         -moving command
+         -firmware restart command
+         -turn motors off
         """
-        if len(sys.argv) < 4:
-            raise CommandError("NOT ENOUGH COMMANDS PASSED TO THE SCRIPT")
-        
-        if sys.argv[1] == "M":
-            self.command = "G1"
-        elif sys.argv[1] == "P":
-            self.command ="G28"
+        if len(self.args) < 2:
+            raise CommandError("NOT ENOUGH COMMANDS")
         else:
-            raise CommandError("WRONG MOVING COMMAND PASSED TO THE SCRIPT")
-        
-        if sys.argv[2] == "X" or sys.argv[2] == "Y":
-            self.motor_name = sys.argv[2]
-        else:
-            raise CommandError("WRONG MOTOR COMMAND")
-        
-        if int(sys.argv[3]) > -90 and int(sys.argv[3]) <= 0:
-            self.coordinates = sys.argv[3]
-        elif int(sys.argv[3]) > 0 and int(sys.argv[3]) < 90:
-            self.coordinates = "+" + sys.argv[3]
-        else:
-            raise CommandError("WRONG COORDINATES TYPE PASSED TO THE SCRIPT")
-        
-        return f"motor name : {self.motor_name}\ncoordinates: {self.coordinates}\ncommand: {self.command}"
+            try:
+                if self.args[1] == "START":
+                    self.starting_command()
+                elif self.args[1] == "RESTART":
+                    self.restart_command()
+                elif self.args[1] == "OFF":
+                    self.off_command()
+                elif (self.args[1].upper() == "X" or self.args[1].upper() == "Y") and (self.args[2]== "+10" or self.args[2] == "-10"):
+                    self.move_command()
+                else:
+                    raise CommandError("NOT ENOUGH COMMANDS")
+            except IndexError:
+                raise CommandError("NOT ENOUGH COMMANDS")
 
 
-    def create_request(self):
-        self.request = f"http://{self.ip_addr}:7125/printer/gcode/script?script={self.command} {self.motor_name}{self.coordinates}"
+    def starting_command(self):
+        resp = self.load_json_response(requests.post(f"http://{self.ip_addr}:7125/printer/gcode/script?script=G28 X0 Y0"))
+        if not resp["result"] == "ok":
+            raise ServerError("BAD SERVER REQUEST")
+        else:
+            resp = self.load_json_response(requests.post(f"http://{self.ip_addr}:7125/printer/gcode/script?script=G91"))
+            if not resp["result"] == "ok":
+                raise ServerError("BAD SERVER REQUEST")
+            else:
+                print("STARTING COMMAND: SUCCESSFULL")
+
+
+    def restart_command(self):
+        resp = self.load_json_response(requests.post(f"http://{self.ip_addr}:7125/printer/gcode/firmware_restart"))
+        print(resp)
 
     
-    def send_request(self):
-        r = requests.post(self.request)
-        r = json.loads(r.text)
-        print(r)
+    def off_command(self):
+        resp = self.load_json_response(requests.post(f"http://{self.ip_addr}:7125/printer/gcode/script?script=M84"))
+        print(resp)
 
-           
+    def move_command(self):
+        axis = self.args[1]
+        angle = self.args[2]
+        resp = self.load_json_response(requests.post(f"http://{self.ip_addr}:7125/printer/gcode/script?script=G1 {axis}{angle}"))
+        print(resp)
+
+
+
+      
 if __name__ == "__main__":       
-
     motors = MotorRequest()
-    print(motors.process_command())
-    motors.create_request()
-    motors.send_request()
+    motors.process_command()
+
