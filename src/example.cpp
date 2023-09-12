6cde84b8f7f7d52997fbf39ee9938bebac10485d
14Og <kolya.kiz@yandex.ru>
2023-09-12 18:05:24 +0300

    added both library and executable file into one project

--- src/example.cpp
+++ src/example.cpp
@@ -8,9 +8,13 @@ int main()
 string ip = "192.168.0.5";
 MotorRequests cbs_stand;
     cbs_stand.StartSession();
+
     cbs_stand.SetCommand(RequestCommands::increase_x_val);
     cbs_stand.SetCommand(RequestCommands::increase_y_val);
+    cbs_stand.SetCommand(RequestCommands::decrease_x_val);
+    cbs_stand.SetCommand(RequestCommands::decrease_y_val);
     cbs_stand.SetCommand(RequestCommands::set_x_val, 30);
     cbs_stand.SetCommand(RequestCommands::set_y_val, -60);
+    cbs_stand.SetCommand(RequestCommands::firmware_restart);
 return 0;
 }
